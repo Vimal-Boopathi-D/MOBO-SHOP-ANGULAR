21d5f4c146300698974d620cb2f62f4a85959a8c
unknown <you@example.com>
2025-11-21 17:59:23 +0530

    upgraded cart page

--- src/app/pages/cart/cart.ts
+++ src/app/pages/cart/cart.ts
@@ -3,16 +3,24 @@ import { CartService } from '../../services/cart.service';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { LottieSectionComponent } from '../../components/lottie-section/lottie-section';
-
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule,LottieSectionComponent],
+  imports: [CommonModule, LottieSectionComponent, FormsModule],
   templateUrl: './cart.html',
   styleUrls: ['./cart.scss']
 })
 export class CartComponent {
+
+  couponCode: string = "";
+  couponApplied = false;
+  invalidCoupon = false;
+
+  couponDiscount = 0;
+  tax = 0;
+  finalAmount = 0;
 
   constructor(
     public cart: CartService,
@@ -22,4 +30,67 @@ export class CartComponent {
   goToCheckout() {
     this.router.navigate(['/checkout']);
   }
+
+  ngDoCheck() {
+    this.calculateAmounts();
+  }
+
+  applyCoupon() {
+    const subtotal = this.cart.totalPrice();
+
+    if (subtotal === 0) {
+      this.invalidCoupon = true;
+      this.couponApplied = false;
+      this.couponDiscount = 0;
+      return;
+    }
+
+    const code = this.couponCode.trim().toUpperCase();
+
+    this.couponApplied = false;
+    this.invalidCoupon = false;
+    this.couponDiscount = 0;
+
+    switch (code) {
+      case "MOBILE10":
+        this.couponApplied = true;
+        this.couponDiscount = subtotal * 0.10;
+        break;
+
+      case "SUPERSAVE":
+        this.couponApplied = true;
+        this.couponDiscount = subtotal * 0.15;
+        break;
+
+      case "NEWUSER":
+        this.couponApplied = true;
+        this.couponDiscount = subtotal * 0.20;
+        break;
+
+      case "FLAT100":
+        this.couponApplied = true;
+        this.couponDiscount = 100;
+        break;
+
+      case "":
+        return;
+
+      default:
+        this.invalidCoupon = true;
+        return;
+    }
+
+    this.calculateAmounts();
+  }
+
+  calculateAmounts() {
+    const subtotal = this.cart.totalPrice();
+
+    if (subtotal === 0) {
+      this.finalAmount = 0;
+      return;
+    }
+    this.finalAmount = subtotal - this.couponDiscount;
+  }
+
 }
